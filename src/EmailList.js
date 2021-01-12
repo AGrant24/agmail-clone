caa75331bd85a6e52686555e72c3f93481a90bb4
AGrant24 <alex.grant.contact@gmail.com>
2021-01-12 21:07:18 +0000

    Message section added, React-Router added

---
+++ src/EmailList.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Checkbox, IconButton } from "@material-ui/core";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import RedoIcon from "@material-ui/icons/Redo";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import KeyboardHideIcon from "@material-ui/icons/KeyboardHide";
+import SettingsIcon from "@material-ui/icons/Settings";
+import PeopleIcon from "@material-ui/icons/People";
+import InboxIcon from "@material-ui/icons/Inbox";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import "./EmailList.css";
+import Section from "./Section";
+import EmailRow from "./EmailRow";
+
+function EmailList() {
+  return (
+    <div className="emailList">
+      <div className="emailList__settings">
+        <div className="emailList__settingsLeft">
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className="emailList__settingsRight">
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="emailList__sections">
+        <Section Icon={InboxIcon} title="primary" color="red" selected />
+        <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+        <Section Icon={LocalOfferIcon} title="Promotions" color="green" />
+      </div>
+      <div className="emailList__list">
+        <EmailRow
+          title="title"
+          subject="subject"
+          description="description"
+          time="00:00"
+        />
+        <EmailRow
+          title="Twitch"
+          subject="Hey Fellow streamer!!!"
+          description="description, description, description, description, description, description, description, description, description,"
+          time="00:00"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default EmailList;
---
+++ src/Mail.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Mail.css";
+
+function Mail() {
+  return (
+    <div className="mail">
+      <h4>mail page</h4>
+    </div>
+  );
+}
+
+export default Mail;
