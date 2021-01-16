b3cd9a0e650978ce1c4a747180f92460c07af636
AGrant24 <alex.grant.contact@gmail.com>
2021-01-16 21:02:36 +0000

    mail screen now pulls data of the actual message

--- src/EmailRow.js
+++ src/EmailRow.js
@@ -5,10 +5,25 @@ import StarBorderOutlinedIcon from "@material-ui/icons/StarBorderOutlined";
 import LabelImportantOutlinedIcon from "@material-ui/icons/LabelImportantOutlined";
 import "./EmailRow.css";
 import { useHistory } from "react-router-dom";
+import { selectMail } from "./features/mailSlice";
+import { useDispatch } from "react-redux";
 
 function EmailRow({ id, title, subject, description, time }) {
   const history = useHistory();
-  const link = () => history.push("/mail");
+  const dispatch = useDispatch();
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
+        subject,
+        description,
+        time,
+      })
+    );
+    history.push("/mail");
+  };
+  // const link = () => history.push("/mail");
   return (
     <div className="emailRow">
       <div className="emailRow__options">
@@ -21,20 +36,20 @@ function EmailRow({ id, title, subject, description, time }) {
         </IconButton>
       </div>
 
-      <h3 className="emailRow__title" onClick={link}>
+      <h3 className="emailRow__title" onClick={openMail}>
         {title}
       </h3>
 
-      <div className="emailRow__message" onClick={link}>
+      <div className="emailRow__message" onClick={openMail}>
         <h4>
           {subject}{" "}
-          <span className="emailRow__description" onClick={link}>
+          <span className="emailRow__description" onClick={openMail}>
             - {description}
           </span>
         </h4>
       </div>
 
-      <div className="emailRow__time" onClick={link}>
+      <div className="emailRow__time" onClick={openMail}>
         {time}
       </div>
     </div>
